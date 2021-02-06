89bf77b8616e4d0526e4d9868ee891a900c9a57a
Mayur Ingle <inglemayur8890@gmail.com>
2021-02-06 23:43:57 +0530

    count related program

---
+++ src/com/onurdesk/count/programs/CountNumberOfWordsInString.java
@@ -0,0 +1,22 @@
+package com.onurdesk.count.programs;
+import java.util.StringTokenizer;
+
+public class CountNumberOfWordsInString
+{
+
+	public static void main(String[] args)
+	{
+		String x="om pakash big bosket etc mfdlkm";
+		StringTokenizer st= new StringTokenizer(x);
+		System.out.println(st.countTokens());
+		while(st.hasMoreTokens())
+		{
+			System.out.print(st.nextToken());
+		}
+	     
+	     String y[]=x.split("");
+	    System.out.println(y);
+
+	}
+
+}
---
+++ src/com/onurdesk/count/programs/CountOfOccuranceEachChar.java
@@ -0,0 +1,39 @@
+package com.onurdesk.count.programs;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class CountOfOccuranceEachChar {
+
+	public static void main(String[] args) {
+	
+		String str="Learn Java Coding";
+		countOfOccuranc1e(str);
+	}
+
+	private static void countOfOccuranc1e(String str) {
+		char[] ch=str.toCharArray();
+		Map<Character,Integer> map=new LinkedHashMap<>();
+		for(Character c:ch)
+		{
+			if(map.containsKey(c))
+			{
+				map.put(c,map.get(c)+1);
+			}else
+			{
+				map.put(c, 1);
+			}
+		}
+		Set<Entry<Character,Integer>> s=map.entrySet();
+		Iterator<Entry<Character,Integer>> itr=s.iterator();
+		while(itr.hasNext())
+		{
+			Map.Entry<Character,Integer> entry=(Map.Entry<Character,Integer>)itr.next();
+	      System.out.println(entry.getKey()+"------"+entry.getValue());
+		}
+		
+	}
+
+}
