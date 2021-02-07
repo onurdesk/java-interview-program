0321a3ddf0b88481ec5c835a541f2c5bf99a8935
Mayur Ingle <inglemayur8890@gmail.com>
2021-02-07 17:39:39 +0530

    Map realted program (onurdesk : http://onurdesk.com)

---
+++ src/com/onurdesk/map/programs/MapExample.java
@@ -0,0 +1,16 @@
+package com.onurdesk.map.programs;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map=new HashMap<>();
+		map.put(100,"OMpraksh");
+		System.out.println(map);
+		map.put(100,"Shiva");
+		System.out.println(map);
+	}
+
+}
---
+++ src/com/onurdesk/map/programs/TreeMapDemp.java
@@ -0,0 +1,25 @@
+package com.onurdesk.map.programs;
+import java.util.*;
+class TreeMapDemo 
+{ 
+	public static void main(String[] args) 
+	{ 
+		TreeMap t=new TreeMap(new MyComparator());
+		t.put("XXX",10); 
+		t.put("AAA",20);  
+		t.put("ZZZ",30); 
+		t.put("LLL",40); 
+		System.out.println(t);
+		//{ZZZ=30, XXX=10, LLL=40, AAA=20} 
+		} 
+	} 
+class MyComparator implements Comparator
+{ 
+	public int compare(Object obj1,Object obj2) 
+	{  
+		String s1=obj1.toString(); 
+		String s2=obj2.toString(); 
+		return s2.compareTo(s1); 
+    }
+
+} 
